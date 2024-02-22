9322f11717c1636a41c6d666fe7781ba4b2fe0ff
Kaasjongetje <cas.hukkelhoven@gmail.com>
2024-02-22 17:16:32 +0100

    Comments bij sveltePreprocess

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// npm install -d svelte-preprocess
+// npm install -d sass
 import sveltePreprocess from 'svelte-preprocess';
 
 // Command to compile the service worker and minify it
